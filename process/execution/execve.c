83d3c3724e51c71bf22d9788f3f3c07696938744
Jianping Duan <static.integer@hotmail.com>
2025-06-05 10:02:55 +0800

    New add execve() programming cases.

---
+++ process/execution/execve.c
@@ -0,0 +1,55 @@
+/*-
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright (c) 2025 Jianping Duan <static.integer@hotmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+#include "unibsd.h"
+
+int
+main(int argc, char *argv[])
+{
+	char *argvec[10];
+	char *envvec[] = { "MYENV1=test01", "MYENV2=test02", NULL };
+
+	if (argc != 2 && strcmp(argv[0], "--help") == 0)
+		errmsg_exit1("Usage: %s pathname\n", argv[0]);
+
+	/* Create an argument list for the new program */
+	if ((argvec[0] = strrchr(argv[1], '/')) != NULL)
+		argvec[0]++;
+	else
+		argvec[0] = argv[1];
+	argvec[1] = "hello world";
+	argvec[2] = "goodbye"
+	argvec[3] = NULL;
+
+	/* Execute the program specified in argv[1] */
+	if (execve(argv[1], argvec, envvec) == -1)	/* more details see execve(2) */
+		errmsg_exit1("execve failed, %s\n", ERR_MSG);
+}
