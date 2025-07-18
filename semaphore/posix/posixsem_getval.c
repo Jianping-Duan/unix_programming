efbd2489bcfea8636fc10f6346ce8c7638ec4a97
Jianping Duan <static.integer@hotmail.com>
2025-07-18 14:56:18 +0800

    Add POSIX Semaphore for wait, post and getvalue

---
+++ semaphore/posix/posixsem_getval.c
@@ -0,0 +1,58 @@
+/*-
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright (c) 2025 Jianping Duan <static.integer@hotmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+#include "unibsd.h"
+#include <semaphore.h>
+
+int
+main(int argc, char *argv[])
+{
+	sem_t *sem;
+	int sval;
+
+	if (argc != 2 || strcmp(argv[1], "--help") == 0)
+		errmsg_exit1("Usage: %s sem-name\n", argv[0]);
+
+	if ((sem = sem_open(argv[1], 0)) == SEM_FAILED)
+		errmsg_exit1("sem_open (%s) failed, %s\n", argv[1], ERR_MSG);
+
+	/*
+	 * The sem_getvalue() function sets the variable pointed to by sval to
+	 * the current value of the semaphore pointed to by sem, as of the time
+	 * that the call to sem_getvalue() is actually run.
+	 */
+	if (sem_getvalue(sem, &sval) == -1)
+		errmsg_exit1("sem_getvalue faild, %s\n", ERR_MSG);
+
+	printf("sval = %d\n", sval);
+
+	exit(EXIT_SUCCESS);
+}
