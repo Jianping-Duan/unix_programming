60f364ee848d13685ec45e9357b8810f6dff3538
Jianping Duan <static.integer@hotmail.com>
2025-07-23 14:04:05 +0800

    Add POSIX shared memory for read and write

---
+++ shmem/posix/posixshm_read.c
@@ -0,0 +1,66 @@
+/*-
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright (c) 2025 Jianping Duan <static.integer@hotmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+#include "unibsd.h"
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int
+main(int argc, char *argv[])
+{
+	int fd;
+	void *addr;
+	struct stat fst;
+	ssize_t sz;
+
+	if (argc != 2 || strcmp(argv[1], "--help") == 0)
+		errmsg_exit1("Usage: %s shm-name\n", argv[0]);
+
+	if ((fd = shm_open(argv[1], O_RDONLY, 0)) == -1)
+		errmsg_exit1("shm_open (%s) failed, %s\n", argv[1], ERR_MSG);
+
+	if (fstat(fd, &fst) == -1)
+		errmsg_exit1("fstat failed, %s\n", ERR_MSG);
+	sz = fst.st_size;
+
+	addr = mmap(NULL, sz, PROT_READ, MAP_SHARED, fd, 0);
+	if (addr == MAP_FAILED)
+		errmsg_exit1("mmap failed, %s\n", ERR_MSG);
+
+	if (close(fd) == -1)
+		errmsg_exit1("close failed, %s\n", ERR_MSG);
+
+	assert(write(STDOUT_FILENO, addr, sz) == sz);
+	write(STDOUT_FILENO, "\n", 1);
+
+	exit(EXIT_SUCCESS);
+}
